97e458178bbe3280b0c9adf3ef74d057dc6a6653
Yingjian <qxlee65@126.com>
2018-08-21 17:39:42 +0800

    Deploy Next.js to gh-pages

---
+++ _next/static/webpack/static/development/pages/index.js.5179ebb3ccc0008c2fed.hot-update.js
@@ -0,0 +1,77 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Header.js":
+/*!******************************!*\
+  !*** ./components/Header.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/yli149/Documents/workspace/next-playground/components/Header.js";
+
+
+var lintStyle = {
+  marginRight: 15
+};
+var logoStyle = {
+  height: 40,
+  length: 100
+};
+
+var Header = function Header() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 14
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 15
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    style: logoStyle,
+    src: "/static/logo.png",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 16
+    }
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 18
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    style: lintStyle,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 19
+    }
+  }, "Home")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_1___default.a, {
+    href: "/".concat("", "/about"),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    style: lintStyle,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    }
+  }, "About")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Header);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.5179ebb3ccc0008c2fed.hot-update.js.map
