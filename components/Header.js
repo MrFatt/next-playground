dc758a960161304894d51779637c53debd7b45a0
Yingjian <qxlee65@126.com>
2018-08-22 11:55:33 +0800

    Add prefix for images

--- components/Header.js
+++ components/Header.js
@@ -14,16 +14,16 @@ const Header = () => {
       }}
     >
       <div className="header-container">
-        <img style={logoStyle} src="/static/logo.png" />
+        <img style={logoStyle} src={`${process.env.BACKEND_URL}/static/logo.png`} />
       </div>
       <div className="header-container">
         <Tabs value={false}>
-          <Tab label="主页" href="/" />
+          <Tab label="主页" href={`${process.env.BACKEND_URL}/`} />
           <Tab label="关于我们" href={`${process.env.BACKEND_URL}/about`} />
         </Tabs>
       </div>
       <a href="https://github.com/TWNTF">
-        <img src="/static/GitHub-Mark-32px.png" />
+        <img src={`${process.env.BACKEND_URL}/static/GitHub-Mark-32px.png`} />
       </a>
       <style global jsx>{`
         .header-container {
