65e7f033465370b6c485afbdb1194cc63f1f1ec8
Yingjian <qxlee65@126.com>
2018-08-22 14:28:05 +0800

    Deploy Next.js to gh-pages

---
+++ _next/static/140ad9b4-9fef-42db-b7a8-677384347b0d/pages/about.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{211:function(e,n,t){e.exports=t(212)},212:function(e,n,t){"use strict";t.r(n);var u=t(0),a=t.n(u),o=t(21);n.default=function(){return a.a.createElement(o.a,null,a.a.createElement("p",null,"This is the about page"))}}},[[211,1,0]]]).default}});
