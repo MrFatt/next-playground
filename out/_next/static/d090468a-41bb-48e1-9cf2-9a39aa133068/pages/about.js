3668a91a56d7fed90762c9b3ec423577079274a7
Yingjian <qxlee65@126.com>
2018-08-22 17:57:40 +0800

    Deploy Next.js to gh-pages

---
+++ out/_next/static/d090468a-41bb-48e1-9cf2-9a39aa133068/pages/about.js
@@ -0,0 +1,1 @@
+module.exports=__NEXT_REGISTER_PAGE("/about",function(){return{page:(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{210:function(e,n,t){e.exports=t(211)},211:function(e,n,t){"use strict";t.r(n);var u=t(0),a=t.n(u),o=t(22);n.default=function(){return a.a.createElement(o.a,null,a.a.createElement("p",null,"This is the about page"))}}},[[210,1,0]]]).default}});
--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ee7af392cec98b6ed697";
+/******/ 	var hotCurrentHash = "f7588b715f61e9f5d2ef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
