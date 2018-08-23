3c085a0c6c35275f4daa8888c80f48eac904dabe
Yingjian <qxlee65@126.com>
2018-08-23 12:14:35 +0800

    Deploy Next.js to gh-pages

---
+++ _next/static/webpack/static/development/pages/index.js.2103e6a12325c2f87f87.hot-update.js
@@ -0,0 +1,75 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Navigation.js":
+/*!**********************************!*\
+  !*** ./components/Navigation.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _material_ui_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @material-ui/core */ "./node_modules/@material-ui/core/index.es.js");
+/* harmony import */ var _material_ui_core_styles__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @material-ui/core/styles */ "./node_modules/@material-ui/core/styles/index.js");
+/* harmony import */ var _material_ui_core_styles__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_material_ui_core_styles__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! next/link */ "./node_modules/next/link.js");
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_3__);
+var _jsxFileName = "/Users/yli149/Documents/workspace/next-playground/components/Navigation.js";
+
+
+
+
+
+var styles = function styles() {
+  return {
+    button: {
+      paddingRight: 20,
+      paddingLeft: 20,
+      fontSize: 18
+    }
+  };
+};
+
+var Navigation = function Navigation(props) {
+  var button = props.classes.button;
+  var backendurl = "";
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 20
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_3___default.a, {
+    href: "".concat(backendurl, "/"),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 21
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_material_ui_core__WEBPACK_IMPORTED_MODULE_1__["ButtonBase"], {
+    className: button,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 22
+    }
+  }, "\u9996\u9875")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(next_link__WEBPACK_IMPORTED_MODULE_3___default.a, {
+    href: "".concat(backendurl, "/about"),
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    }
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_material_ui_core__WEBPACK_IMPORTED_MODULE_1__["ButtonBase"], {
+    className: button,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 25
+    }
+  }, "\u5173\u4E8E\u6211\u4EEC")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(_material_ui_core_styles__WEBPACK_IMPORTED_MODULE_2__["withStyles"])(styles)(Navigation));
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.2103e6a12325c2f87f87.hot-update.js.map
