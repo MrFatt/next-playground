b4205c5832d488861fb68e7cd90f6d1acee648f9
Yingjian Li <yingjli@thoughtworks.com>
2018-08-28 00:01:01 +0800

    WIP: about page TODO: get url from member api

--- components/Members.js
+++ components/Members.js
@@ -3,14 +3,18 @@ import MemberInfo from "./MemberInfo";
 const memberContainer = {
   marginLeft: 20,
   marginRight: 20,
+  marginTop: 40,
   display: "flex",
   flexDirection: "row",
   justifyContent: "space-between"
 };
 export default props => (
-  <div style={memberContainer}>
-    {props.nodes.map(node => (
-      <MemberInfo key={node.id} member={node} />
-    ))}
+  <div>
+    <h2>团队成员:</h2>
+    <div style={memberContainer}>
+      {props.nodes.map(node => (
+        <MemberInfo key={node.id} member={node} />
+      ))}
+    </div>
   </div>
 );
