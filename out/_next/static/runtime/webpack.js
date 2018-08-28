83da722a21c0ff7d1b6a3e842460a9ca5c22c842
Yingjian <qxlee65@126.com>
2018-08-28 11:18:08 +0800

    Deploy Next.js to gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "309bee5a2ce5b8592266";
+/******/ 	var hotCurrentHash = "77b84214602a0901ca1a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
