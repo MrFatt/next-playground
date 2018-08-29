9c6ff95e6e1fe980d9c9d265e9a69d4a6f4eb792
Yingjian <qxlee65@126.com>
2018-08-29 15:13:44 +0800

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rm -rf node_modules/.cache
+node_modules/.bin/next build && node_modules/.bin/next export
+touch out/.nojekyll
+buildID=$(cat .next/BUILD_ID)
+sed -i '' '/authorization/d' ./out/_next/static/development/pages/_app.js
+sed -i '' 's/authorization:\"Bearer \".concat(\"[0-9a-zA-Z]*\")//' out/_next/static/$buildID/pages/_app.js && git add out/
+git commit -m "Deploy Next.js to gh-pages, build id: $buildID"
+git subtree push --prefix out origin gh-pages
